fc20dd22520296f9f16c3f9b625ab28c230a9356
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-23 20:13:32 +0300

    navbar css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import { Home } from './component/Home';
-import './App.css';
+import { Home } from "./component/Home";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Routes exact path="/" component={Home} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
